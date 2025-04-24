a7971707ac4b7e48fee339559e88b34fa73a7676
Boundless-State <timmy.windahl@gmail.com>
2025-04-24 11:04:20 +0200

    trying to fix the missing api problem on azure

--- WebApi/Middleware/ApiKeyMiddleware.cs
+++ WebApi/Middleware/ApiKeyMiddleware.cs
@@ -5,29 +5,16 @@ using System.Threading.Tasks;
 public class ApiKeyMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly IConfiguration _configuration;
+    private readonly string _apiKey;
 
     public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
     {
         _next = next;
-        _configuration = configuration;
+        _apiKey = configuration["ApiKey"];
     }
 
-    public async Task InvokeAsync(HttpContext context)
+    public async Task Invoke(HttpContext context)
     {
-        var path = context.Request.Path.Value;
-        if (path.StartsWith("/swagger") ||
-            path.StartsWith("/favicon.ico") ||
-            path.EndsWith(".js") ||
-            path.EndsWith(".css") ||
-            path.EndsWith(".map") ||
-            path.EndsWith(".json") ||
-            path.EndsWith(".html"))
-        {
-            await _next(context);
-            return;
-        }
-
         if (!context.Request.Headers.TryGetValue("x-api-key", out var extractedApiKey))
         {
             context.Response.StatusCode = 401;
@@ -35,9 +22,7 @@ public class ApiKeyMiddleware
             return;
         }
 
-        var configuredApiKey = _configuration.GetValue<string>("ApiKey");
-
-        if (!configuredApiKey.Equals(extractedApiKey))
+        if (!_apiKey.Equals(extractedApiKey))
         {
             context.Response.StatusCode = 401;
             await context.Response.WriteAsync("Ogiltig API-nyckel");
@@ -47,5 +32,3 @@ public class ApiKeyMiddleware
         await _next(context);
     }
 }
-
-
